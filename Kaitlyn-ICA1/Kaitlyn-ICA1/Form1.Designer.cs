71487c76e5f3a0f900e1b75c03eb0909a7a64478
kmurphy23 <32750107+kmurphy23@users.noreply.github.com>
2018-01-10 13:11:10 -0700

    Attempt2

---
+++ Kaitlyn-ICA1/Kaitlyn-ICA1/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Kaitlyn_ICA1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.BrightUp = new System.Windows.Forms.ToolStripButton();
+            this.BrightDown = new System.Windows.Forms.ToolStripButton();
+            this.Open = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Open,
+            this.BrightUp,
+            this.BrightDown,
+            this.toolStripSeparator1,
+            this.toolStripProgressBar1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 442);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(494, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // BrightUp
+            // 
+            this.BrightUp.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.BrightUp.Image = ((System.Drawing.Image)(resources.GetObject("BrightUp.Image")));
+            this.BrightUp.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BrightUp.Name = "BrightUp";
+            this.BrightUp.Size = new System.Drawing.Size(76, 22);
+            this.BrightUp.Text = "Brigth +10%";
+            this.BrightUp.Click += new System.EventHandler(this.BrightUp_Click);
+            // 
+            // BrightDown
+            // 
+            this.BrightDown.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.BrightDown.Image = ((System.Drawing.Image)(resources.GetObject("BrightDown.Image")));
+            this.BrightDown.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BrightDown.Name = "BrightDown";
+            this.BrightDown.Size = new System.Drawing.Size(70, 22);
+            this.BrightDown.Text = "Bright-10%";
+            this.BrightDown.Click += new System.EventHandler(this.BrightDown_Click);
+            // 
+            // Open
+            // 
+            this.Open.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.Open.Image = ((System.Drawing.Image)(resources.GetObject("Open.Image")));
+            this.Open.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(40, 22);
+            this.Open.Text = "Open";
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 22);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(494, 442);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 467);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton Open;
+        private System.Windows.Forms.ToolStripButton BrightUp;
+        private System.Windows.Forms.ToolStripButton BrightDown;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Kaitlyn-ICA1/Kaitlyn-ICA1/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Kaitlyn_ICA1
+{
+    public partial class Form1 : Form
+    {
+        //original version of the picture DONT MESS WITH THIS ONE
+        Bitmap original;
+        //now we have the delegate were gonna need to use 
+        private delegate void delVoidBit(Bitmap map);
+
+        public struct Choice
+        {
+            //struct has 2 things our bitmap and our choice of brightness
+            public Bitmap BM;
+            public float BrightVal;
+            public Choice(Bitmap bm, float fVal)
+            {
+                BM = new Bitmap(bm);
+                BrightVal = fVal;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //take the picture from the open file dialog and then plop it in the bitmap
+                pictureBox1.Load(openFileDialog1.FileName);
+                original = new Bitmap(pictureBox1.Image);
+            }
+        }
+
+        private void BrightUp_Click(object sender, EventArgs e)
+        {
+            //assign the value of the brightness
+            Choice newC = new Choice(original,1.1f);
+            toolStripProgressBar1.Value = 0;
+            ParameterizedThreadStart pts = new ParameterizedThreadStart(Adjust);
+
+            //start the thread!
+            Thread start = new Thread(pts);
+            start.IsBackground = true;
+            start.Start(newC);
+
+        }
+
+        private void BrightDown_Click(object sender, EventArgs e)
+        {
+            //assign the value of the brightness
+            Choice newC = new Choice(original, 0.9f);
+            toolStripProgressBar1.Value = 0;
+            ParameterizedThreadStart pts = new ParameterizedThreadStart(Adjust);
+
+            //start the thread!
+            Thread start = new Thread(pts);
+            start.IsBackground = true;
+            start.Start(newC);
+        }
+
+        private void Adjust(object Ad)
+        {
+
+            //creates an instance of the choice struct
+            Choice choice = (Choice)Ad;
+
+            Bitmap pic = choice.BM;
+            //a variable that we will store the value of the pixel color (does that sentecne make sense?)
+            Color c;
+            float pixelR;
+            float pixelG;
+            float pixelB;
+            //filtes through each pixel in the width
+            for (int y = 0; y < original.Height; y++)
+            {
+                //filters through each pixel in the height
+                for (int x = 0; x < original.Width; x++)
+                {
+                    pixelR = pic.GetPixel(x, y).R;
+                    pixelG = pic.GetPixel(x, y).G;
+                    pixelB = pic.GetPixel(x, y).B;
+
+                    if (choice.BrightVal == 1.1f)
+                    {
+                        pixelR = Math.Min(pixelR *= choice.BrightVal, 255);
+                        pixelG = Math.Min(pixelG *= choice.BrightVal, 255);
+                        pixelB = Math.Min(pixelB *= choice.BrightVal, 255);
+                    }
+
+                    else if (choice.BrightVal == 0.9f)
+                    {
+                        pixelR = Math.Max(pixelR *= choice.BrightVal, 0);
+                        pixelG = Math.Max(pixelG *= choice.BrightVal, 0);
+                        pixelB = Math.Max(pixelB *= choice.BrightVal, 0);
+                    }
+                    
+                    c = Color.FromArgb((int)(pixelR), (int)(pixelG), (int)(pixelB));
+                    pic.SetPixel(x, y, c);
+                }
+            }
+            
+            //invokes the delegate to call the set map method
+            Invoke(new delVoidBit(SetMap), pic);
+
+        }
+
+        //thread meth
+        private void SetMap(Bitmap map)
+        {
+            pictureBox1.Image = map;
+        }
+
+    }
+}
